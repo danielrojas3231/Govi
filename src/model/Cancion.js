10e3359fdd920ad46e94661d909de2d25f96c608
Nicolas Montero <nicolasmontero42@gmail.com>
2019-11-21 15:47:04 -0500

    Model
    Se crearon las clases del model

---
+++ src/model/Cancion.js
@@ -0,0 +1,16 @@
+class Cancion{
+
+    constructor(app){
+
+        this.app = app;
+        this.nombre = nombre;
+        this.autor = autor;
+        this.genero = genero;
+        this.album = album;
+        this.imagen = imagen;
+        this.duracion = duracion;
+        this.fecha = fecha;
+
+    }
+
+}
---
+++ src/model/Logica.js
@@ -0,0 +1,52 @@
+class Logica{
+
+    constructor(app){
+        this.app=app;
+        this.usuarios=[];
+        this.canciones=[];
+
+
+    }
+
+    cargarInfoCanciones(){
+
+
+
+    }
+
+    cargarImagenDeCancion(){
+
+
+
+    }
+
+    cargarAudioDeCancion(){
+
+
+
+    }
+
+    agregarCancion(){
+
+
+
+    }
+
+    crearCuenta(){
+
+
+
+    }
+
+    iniciarSesion(){
+
+
+    }
+
+    cerrarSesion(){
+
+        
+    }
+
+
+}
